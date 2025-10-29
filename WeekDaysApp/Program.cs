38f25ef0efa5696f746961798ff16ec8a5793e52
Mohammad Adrien Jalayer <adrianjalayer@gmail.com>
2025-10-29 02:57:18 -0700

    Add files via upload

---
+++ WeekDaysApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        string[] daysOfWeek = {
+            "sat",    // index 0
+            "sun",  // index 1
+            "mon",  // index 2
+            "tue", // index 3
+            "wed",// index 4
+            "thu",// index 5
+            "fri"     // index 6
+        };
+
+        Console.Write("Enter a number:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number >= 1 && number <= 7)
+        {
+            Console.WriteLine("Today is: " + daysOfWeek[number - 1]);
+        }
+        else
+        {
+            Console.WriteLine("!END");
+        }
+    }
+}
